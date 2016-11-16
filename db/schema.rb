e298b2f1449827e284fcf6ddbb4507dc1cbd82d5
iankhor <iankhorbc.thecoder@gmail.com>
2016-11-16 14:43:11 +1100

    added specialty model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116033204) do
+ActiveRecord::Schema.define(version: 20161116034123) do
 
   create_table "allergies", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 20161116033204) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.index ["blood_type_id"], name: "index_experiments_on_blood_type_id"
+  end
+
+  create_table "genders", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "marital_statuses", force: :cascade do |t|
