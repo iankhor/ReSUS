d7c198b6b7f56be00a759af34d26b24b7e6bc16f
iankhor <iankhorbc.thecoder@gmail.com>
2016-11-17 14:07:45 +1100

    updated redirected page after sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :patient_profiles
   resources :experiments
   root 'home#index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
