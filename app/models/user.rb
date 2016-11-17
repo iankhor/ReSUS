8ca2437b2b3a1b403b3d410fbf1e0649b2f1153a
iankhor <iankhorbc.thecoder@gmail.com>
2016-11-17 13:27:02 +1100

    updated sign up form to allow user to define role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  ROLES = %w[i_want_to_seek_treatment i_want_to_help].freeze
+
 end
