f5289861f817693e4b5fac067d7a0245e759400d
iankhor <iankhorbc.thecoder@gmail.com>
2016-11-22 02:48:58 +1100

    update logo and landing page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( new-age.min.css )
+Rails.application.config.assets.precompile += %w( new-age.min.js )
