52231a7f2786c241a962d20761be5e4fd365642a
iankhor <iankhorbc.thecoder@gmail.com>
2016-11-21 17:58:56 +1100

    updated functionarlity of patient dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,13 @@
 class DashboardController < ApplicationController
 
     def index
-        case current_user.role.to_i
-        when 1
-            @dashboard = Listing.all
-        when 2
+        # case current_user.role.to_i
+        # when 1
+        #     @dashboard = Listing.all
+        # when 2
+        #     @dashboard = Quote.all
+        # end
             @dashboard = Quote.all
-        end
     end
 
     
