6160ebc95e06b7993067dcae3d6650cb26676d4b
iankhor <iankhorbc.thecoder@gmail.com>
2016-11-23 10:09:55 +1100

    updated application controller rescue code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # rescue_from ActiveRecord::RecordNotFound do
-  #   flash[:warning] = 'Resource not found.'
-  #   redirect_back_or root_path
-  # end
+  rescue_from ActiveRecord::RecordNotFound do
+    flash[:warning] = 'Resource not found.'
+    redirect_back_or root_path
+  end
 
   def redirect_back_or(path)
     redirect_to request.referer || path
