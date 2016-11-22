3c95343227ab7464de13c10c33005b71a22b26cc
iankhor <iankhorbc.thecoder@gmail.com>
2016-11-22 15:26:48 +1100

    updated dashboard so users can only see their entries

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,9 +3,9 @@ class DashboardController < ApplicationController
     def index
         case current_user.role.to_i
         when 1
-            @dashboard = Listing.all
+            @dashboard = Quote.where(listing: Listing.where(patient: current_user))
         when 2
-            @dashboard = Quote.all
+            @dashboard = Quote.where(practitioner: current_user)
         end
         # @dashboard = Quote.all
     end
