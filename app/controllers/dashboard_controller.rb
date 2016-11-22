0a8e5dc272e843ec40d6d879ab851415253f0b86
iankhor <iankhorbc.thecoder@gmail.com>
2016-11-22 15:19:49 +1100

    fixed minor log in bug

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,13 @@
 class DashboardController < ApplicationController
 
     def index
-        # case current_user.role.to_i
-        # when 1
-        #     @dashboard = Listing.all
-        # when 2
-        #     @dashboard = Quote.all
-        # end
+        case current_user.role.to_i
+        when 1
+            @dashboard = Listing.all
+        when 2
             @dashboard = Quote.all
+        end
+        # @dashboard = Quote.all
     end
 
     
