9094543c37ef1bc9bb882fa51960ca42616068cb
iankhor <iankhorbc.thecoder@gmail.com>
2016-11-24 04:24:10 +1100

    updated patient dashboard bug

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,9 @@ class DashboardController < ApplicationController
     def index
         case current_user.role.to_i
         when 1
-            @dashboard = Quote.where(listing: Listing.where(patient: current_user))
+            # @dashboard = Quote.where(listing: Listing.where(patient: current_user))
+             @dashboard = Listing.where(patient: current_user)
+            
         when 2
             @dashboard = Quote.where(practitioner: current_user)
         end
