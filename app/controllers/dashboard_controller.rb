b0bdff8b44c71ea7d381b8b2a77c6f93b0fc8491
iankhor <iankhorbc.thecoder@gmail.com>
2016-11-24 11:14:02 +1100

    updated database

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,11 +8,13 @@ class DashboardController < ApplicationController
              @dashboard = Listing.where(patient: current_user)
             
         when 2
-            specialty = PractitionerProfile.find_by()
-             @dashboard = Listing.where(patient: current_user)
+             specialty = PractitionerProfile.find_by(user_id: current_user.id).specialty_id
+
+             condition_treated = Seek.find_by(specialty: specialty)
+
+             @dashboard = Listing.where(medical_condition: condition_treated)
             # @dashboard = Quote.where(practitioner: current_user)
         end
-        # @dashboard = Quote.all
     end
 
     
